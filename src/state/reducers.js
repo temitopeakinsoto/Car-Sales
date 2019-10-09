ad5a8f4dd38e3ea4b5a5a14a60bebb3ab2aca0ba
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-09 19:19:17 +0100

    refactor code to remove all hard coded stuff

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,4 +1,4 @@
-
+import * as types from './actionTypes';
 const initialState = {
     additionalPrice: 0, 
     car: {
@@ -6,7 +6,7 @@ const initialState = {
     name: '2019 Ford Mustang',
     image:
         'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-    features: [{ id: 1, name: 'V-6 engine', price: 1500 }]
+    features: []
     },
     store: [
     { id: 1, name: 'V-6 engine', price: 1500 },
